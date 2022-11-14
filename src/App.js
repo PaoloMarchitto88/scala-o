d9b63a3df2ee3e92ca770205d3bb6015a469ca0a
Paolo Marchitto <p.marchitto@exolab.it>
2022-11-14 17:27:47 +0100

    test bootstrap

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ciao
         </p>
+        <button className="btn btn-primary">Button</button>
         <a
           className="App-link"
           href="https://reactjs.org"
